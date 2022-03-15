8c47d3ac8024886530279f89f69862d2f02da7fa
Momen Bazzar <momen.b2001@gmail.com>
2022-03-15 21:19:57 +0200

    Exercise2: Done

--- src/LINQ/Program.cs
+++ src/LINQ/Program.cs
@@ -11,15 +11,24 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int> {2, 4, 7, 12, 6};
-            List<int> squares = new List<int> {4, 16, 49, 144, 36};
+            List<int> numbers = new List<int> { 2, 4, 8, 12, 6 };
+            List<int> squares = new List<int> { 4, 16, 64, 144, 36 };
 
             System.Console.WriteLine(TestForSquares(numbers, squares));
+
+            List<string> words = new List<string> { "yield", "Randa", "Othman",  "Hello", "Momen"};
+            System.Console.WriteLine(GetTheLastWord(words));
         }
 
         public static bool TestForSquares(IEnumerable<int> numbers, IEnumerable<int> squares)
         {
-            return true;
+            return numbers.Select(c => c * c).SequenceEqual(squares);
+        }
+
+        public static string? GetTheLastWord(IEnumerable<string> words)
+        {
+            var resultWord = words.Where(word => word.Contains("e")).OrderBy(word => word).LastOrDefault();
+            return $"The last word is {resultWord}";
         }
     }
 }
