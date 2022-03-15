7f4134419321ebdd9f82192faddab795b3929e08
Momen Bazzar <momen.b2001@gmail.com>
2022-03-15 20:42:12 +0200

    Start First Task

--- src/LINQ/Program.cs
+++ src/LINQ/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace LINQ;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-internal class Program
+namespace LINQ
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Me!");
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> {2, 4, 7, 12, 6};
+            List<int> squares = new List<int> {4, 16, 49, 144, 36};
+
+            System.Console.WriteLine(TestForSquares(numbers, squares));
+        }
+
+        public static bool TestForSquares(IEnumerable<int> numbers, IEnumerable<int> squares)
+        {
+            return true;
+        }
     }
 }
